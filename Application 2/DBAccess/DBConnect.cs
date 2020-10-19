ade43ef84ee93cd0da84bdbe0100950d24ce4c0e
Annice Strömberg <annice.stromberg@gmail.com>
2020-10-20 01:28:14 +0200

    Update DBConnect.cs

--- Application 2/DBAccess/DBConnect.cs
+++ Application 2/DBAccess/DBConnect.cs
@@ -3,6 +3,19 @@ using Microsoft.Extensions.Configuration;
 using App2.Models;
 using System.Data.SqlClient;
 
+/*
+Copyright (c) 2020 Annice Strömberg – Annice.se
+
+This script is MIT (Massachusetts Institute of Technology) licensed, which means that
+permission is granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files to deal in the software without restriction. This
+includes, without limitation, the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the software, and to permit persons to whom the software
+is furnished to do so subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the software.
+*/
 namespace App2.DBAccess
 {
     /// <summary>
