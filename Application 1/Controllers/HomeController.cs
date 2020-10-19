fa0463a857ad0b5993c52722d1ea8bec7ac5c7f4
Annice Strömberg <annice.stromberg@gmail.com>
2020-10-20 01:27:29 +0200

    Update HomeController.cs

--- Application 1/Controllers/HomeController.cs
+++ Application 1/Controllers/HomeController.cs
@@ -5,6 +5,19 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using MVCApp1.Models;
 
+/*
+Copyright (c) 2020 Annice Strömberg – Annice.se
+
+This script is MIT (Massachusetts Institute of Technology) licensed, which means that
+permission is granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files to deal in the software without restriction. This
+includes, without limitation, the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the software, and to permit persons to whom the software
+is furnished to do so subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the software.
+*/
 namespace MVCApp1.Controllers
 {
     /// <summary>
